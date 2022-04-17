3c23b1e32fde951e696987692c64b643f55fa2d8
ArulMC123 <99807639+ArulMC123@users.noreply.github.com>
2022-04-18 04:43:39 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -11,18 +11,18 @@ global.APIKeys = {
 }
 
 //===> UBAH DISINI
-global.namaowner = 'Ryuuka Team' //Ganti Jadi Namamu
-global.owner = ['6285609233482','6283891921787'] //Kalo Mau Lebih Tambahin Aja
-global.nomerowner = '6285609233482' //Owner Utama Istilahnya
-global.namabot = 'RyuBotz MD' //Ganti Jadi Nama Botmu
-global.packname = '© Ryubotz MD' //Sticker Weem
-global.author = 'Ryubotz By Ryuuka Team' //Sticker Weem
+global.namaowner = 'Sky Team' //Ganti Jadi Namamu
+global.owner = ['6289510702581','6285339347510','6288219647445','62882009117779'] //Kalo Mau Lebih Tambahin Aja
+global.nomerowner = '6288219647445' //Owner Utama Istilahnya
+global.namabot = 'SkyBotz' //Ganti Jadi Nama Botmu
+global.packname = '© SkyBotz' //Sticker Weem
+global.author = 'SkyBotz By Sky Team' //Sticker Weem
 global.webme = 'https://api-ryubotz.herokuapp.com'
 global.sessionName = 'session'
 global.bg_welcome = 'https://telegra.ph/file/528b594d5d91c08482515.jpg' //Max 30kb Agar Tidak Delay
-global.youtube = 'https://youtube.com/c/RyuukaBotz'
-global.github = 'https://github.com/R-TEAM94'
-global.instagram = 'https://instagram.com/fandyyy._'
+global.youtube = 'https://youtube.com/channel/UCJh9I1GEVJk8qQWGs1t09eQ'
+global.github = 'https://github.com/ArulMC123'
+global.instagram = 'https://www.instagram.com/asrul54365?r=nametag'
 
 //===> BACKGROUND SETIAP MENU
 global.bg1 = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=water-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
@@ -35,14 +35,14 @@ global.sp = '⭔'
 
 //===> EDIT SESUAI KEMAUANMU
 global.mess = {
-admin: '*LU ADMIN? 🤨*',
-botAdmin: '*BOT BUKAN ADMIN 😔*',
-owner: '*LU BUKAN OWNER GW 😠*',
-group: '*KHUSUS GROUP 😏*',
-private: '*KHUSUS PRIVATE CHAT*',
-bot: '*KHUSUS PENGGUNA NOMER BOT*',
-wait: '*SABAR SEDANG DI PROSES ...*',
-done: '*DONE NGAB 🤗*',
+admin: '*Maaf, Kamu Bukan Admin*',
+botAdmin: '*SkyBotz Bukan Admin !!!*',
+owner: '*Maaf, Fitur Khusus SkyTeam*',
+group: '*KHUSUS GROUP !!!*',
+private: '*KHUSUS PRIVATE CHAT !!!*',
+bot: '*KHUSUS PENGGUNA NOMER BOT !!!*',
+wait: '*Proses ...*',
+done: '*DONE !!!*',
 }
 global.thumb = fs.readFileSync('./media/ryuu.jpg')
     
